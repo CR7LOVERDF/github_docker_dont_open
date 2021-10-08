91ad1de3fde1257c228c16d044e030c4cfcbc110
akashvnm <89898135+akashvnm@users.noreply.github.com>
2021-10-08 12:16:41 +0530

    Update __init__.py

--- LuciferMoringstar_Robot/__init__.py
+++ LuciferMoringstar_Robot/__init__.py
@@ -15,21 +15,6 @@ HELP = """
 
 /start : check if am alive or dead
 /about : About Me
-
-<u>Bot Owner Only</u>
-
-  /broadcast Replay Any Message or Media
-  /stats user Status
-  /ban_user  click ban_user more info
-  /unban_user click unban_user more info
-  /banned_users Banned User Details
-  /total How Many Files Added In Database
-  /logger  Get Logs
-  /delete Delete File From Database
-
-[➡️How To Add Old Files For database](https://youtu.be/mtu5hgW-l_o)
-
-@Mo_Tech_YT
 """
 
 ABOUT = """
